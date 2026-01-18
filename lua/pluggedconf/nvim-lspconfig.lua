68ebca01f36e4360bcdfcdc916cbab494ed41377
Łukasz Anuszkiewicz <odysey84@o2.pl>
2026-01-18 14:08:25 +0100

    changed pylsp mess

--- lua/pluggedconf/nvim-lspconfig.lua
+++ lua/pluggedconf/nvim-lspconfig.lua
@@ -153,20 +153,29 @@ vim.lsp.enable('html')
 vim.lsp.enable('jsonls')
 vim.lsp.enable('marksman')
 vim.lsp.enable('cssls')
-vim.lsp.enable('pyright')
 vim.lsp.config('pylsp', {
     settings = {
         pylsp = {
             plugins = {
+                jedi_completion = { enabled = true },
+                jedi_definition = { enabled = true },
+                jedi_hover = { enabled = true },
+                jedi_references = { enabled = true },
+                jedi_rename = { enabled = true },
+                jedi_signature_help = { enabled = true },
+                jedi_symbols = { enabled = true },
+                pyflakes = { enabled = true },
                 pycodestyle = {
                     ignore = { 'E501' },
                     maxLineLength = 100
                 },
                 mccabe = { enabled = true },
-                flake8 = {
-                    enabled = true,
-                    ignore = { 'E501' }
-                }
+
+                flake8 = { enabled = false },
+                pylint = { enabled = false },
+                rope_autoimport = { enabled = false },
+                rope_completion = { enabled = false },
+                pydocstyle = { enabled = false },
             }
         }
     }
